f4238c98ead97403dae5cb100dac24033804e46b
fqw <931823794@qq.com>
2019-02-15 11:36:45 +0800

    首页布局容器使用和样式调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>home--</div>
+  <el-container class="container">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -7,4 +13,15 @@ export default {}
 </script>
 
 <style>
+.container {
+  height: 100%;
+  background-color: red;
+}
+.aside {
+  background-color: yellow;
+}
+
+.main {
+  background-color: green;
+}
 </style>
