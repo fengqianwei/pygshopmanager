ac7ebdc52cfecdf02f18b6d104d229e6e95236d3
fqw <931823794@qq.com>
2019-02-15 10:08:14 +0800

    引入登录组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>login-----</div>
+  <div>
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button class="login-btn" type="primary">登录</el-button>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      labelPosition: 'top',
+      formdata: {
+        username: '',
+        password: '',
+        type: ''
+      }
+    }
+  }
+}
 </script>
 
 <style>
