1b0dfb5d74ea1b105442af2b9f88ec94374e9b63
fqw <931823794@qq.com>
2019-02-17 14:44:37 +0800

    调试代码

--- src/components/users.vue
+++ src/components/users.vue
@@ -24,9 +24,9 @@
       <el-table-column prop="username" label="姓名" width="120"></el-table-column>
       <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
       <el-table-column prop="mobile" label="电话" width="140"></el-table-column>
-      <el-table-column prop="name" label="创建日期" width="140"></el-table-column>
-      <el-table-column prop="name" label="用户状态" width="140"></el-table-column>
-      <el-table-column prop="name" label="操作" width="200"></el-table-column>
+      <el-table-column prop="create_time" label="创建日期" width="140"></el-table-column>
+      <el-table-column prop="mg_state" label="用户状态" width="140"></el-table-column>
+      <el-table-column prop="role_name" label="操作" width="200"></el-table-column>
     </el-table>
     <!-- 分页 -->
   </el-card>
@@ -48,14 +48,6 @@ export default {
   methods: {
     // 获取表格数据
     async getTableData() {
-      // 除了登录请求.其他所有请求都需要授权->
-      // 在发送请求之前,先设置请求头{Authorization:token值}
-      // 设置请求头headers -> 发送请求用的是axios->找axiosAPI有没有可以设置请求头->看文档
-      // {
-      //   ContentType:text/html,
-      //   Authorization:?
-      // }
-
       const AUTH_TOKEN = localStorage.getItem("token");
       this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
 
@@ -64,7 +56,14 @@ export default {
           this.pagesize
         }`
       );
-      console.log(res);
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        this.list = data.users;
+        console.log(this.list);
+      }
     }
   }
 };
