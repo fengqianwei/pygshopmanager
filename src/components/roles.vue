f3d92e4dd14d8906fd46cef589b481b9cdf89374
fqw <931823794@qq.com>
2019-02-19 09:48:51 +0800

    三级权限

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -3,10 +3,42 @@
     <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
     <el-button class="btn" type="success">添加用户</el-button>
     <!-- 表格 -->
-    <el-table :data="roles" style="width: 100%">
-      <el-table-column type="index" label="#" width="80"></el-table-column>
-      <el-table-column prop="roleName" label="角色名称" width="120"></el-table-column>
-      <el-table-column prop="roleDesc" label="角色描述" width="140"></el-table-column>
+    <el-table height="450px" :data="roles" style="width: 100%">
+      <el-table-column type="expand" width="80">
+        <template slot-scope="scope">
+          <el-row class="level1" v-for="(item1, i) in scope.row.children" :key="item1.id">
+            <el-col :span="4">
+              <el-tag type="success" closable>{{item1.authName}}</el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row class="level2" v-for="(item2, i) in item1.children" :key="item2.id">
+                <el-col :span="4">
+                  <el-tag type="warning" closable>{{item2.authName}}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    closable
+                    type="danger"
+                    class="level3"
+                    v-for="(item3, i) in item2.children"
+                    :key="item3.id"
+                  >{{item3.authName}}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+          <el-row v-if="scope.row.children.length===0">
+            <el-col>
+              <span>未分配权限</span>
+            </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
+      <el-table-column type="index" label="#" width="180"></el-table-column>
+      <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
+      <el-table-column prop="roleDesc" label="角色描述" width="200"></el-table-column>
 
       <el-table-column label="操作" width="200">
         <template slot-scope="scope">
@@ -60,5 +92,9 @@ export default {
 .btn {
   margin-top: 20px;
   margin-bottom: 20px;
+}
+.level1,
+.level2 {
+  margin-bottom: 10px;
 }
 </style>
